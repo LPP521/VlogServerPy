c8b5c901a453af21d562939e1eb4c88eb6e8167a
shimachao <shimachao@yeah.net>
2013-06-26 17:28:01 +0800

    3

--- request_handler.py
+++ request_handler.py
@@ -19,24 +19,25 @@ def friends(tcpconn):
 
 #插入视频日志记录
 def insertVideoRecord(c):
-	title,filename,author,explain = c.recv(1024).split(',')
+	buf = c.recv(1024)
+	print buf
+	title,filename,author,explain =buf.split(',')  # c.recv(1024).split(',')
+	print explain
 	picture = filename[:-3] + 'jpg' #图片名称和视频名称就后缀名不一样
 	videourl = '/var/videohome/'+filename
 	cur.execute('insert into vl_video(video_name,video_picture,video_url,video_author,video_date,video_explain) \
-				VALUES(%s,%s,%s,%s,%s,%s)',(title,picture,videourl,author,'NOW()',explain))
+				VALUES(%s,%s,%s,%s,NOW(),%s)',(title,picture,videourl,author,explain))
+	conn.commit()
 
 def recvFile(c):
 	fileType,filename = c.recv(24).split(',')
 	#根据文件类型觉定文件的保存路径
-	print fileType
-	print filename
 	path = ''
 	if fileType =='video':
 		path = r'/var/videohome/'+filename
 	else:
 		path = r'/var/imagehome/'+filename
 	#开始接收文件
-	print path
 	f = open(path,'wb')
 	while True:
 		buf = c.recv(1024)
