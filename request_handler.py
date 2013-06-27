e312de41373e876468406883d61fdcedeac9263c
shimachao <shimachao@yeah.net>
2013-06-27 20:17:53 +0800

    上传和插入视频记录都实现了

--- request_handler.py
+++ request_handler.py
@@ -6,29 +6,46 @@
 import socket
 import MySQLdb
 import MySQLdb.cursors
+import struct
 
-conn = MySQLdb.connect(host='localhost',user='root',passwd='',db='vlog_v1',cursorclass=MySQLdb.cursors.DictCursor)
+conn = MySQLdb.connect(host='localhost',user='root',passwd='',db='vlog_v1',charset='utf8',cursorclass=MySQLdb.cursors.DictCursor)
 cur = conn.cursor()
 #请求好友列表
 def friends(tcpconn):
-	'根用户id查询好友列表'
-	userid = tcpconn.recv(16)
-	cur.execute('select userfriend,friends_name from vl_friend where user = %s',userid)
-	res = cur.fetchall()
-	print res
+	'根用户email查询好友列表'
+	user_email = tcpconn.recv(50)
+	cur.execute('select userfriend,friends_name from vl_friend where user = %s',user_email)
+	buf = u''
+	for item in cur.fetchall():
+		buf = buf + item['userfriend'] + ',' + item['friends_name'] + ','
+	buf = buf[:-1]
+	buf = buf.encode('utf8')
+	print len(buf)
+	buf = struct.pack('B',len(buf))+buf
+	print repr(buf)
+	tcpconn.send(buf)
+
 
 #插入视频日志记录
 def insertVideoRecord(c):
 	buf = c.recv(1024)
 	print buf
 	title,filename,author,explain =buf.split(',')  # c.recv(1024).split(',')
-	print explain
 	picture = filename[:-3] + 'jpg' #图片名称和视频名称就后缀名不一样
 	videourl = '/var/videohome/'+filename
 	cur.execute('insert into vl_video(video_name,video_picture,video_url,video_author,video_date,video_explain) \
 				VALUES(%s,%s,%s,%s,NOW(),%s)',(title,picture,videourl,author,explain))
 	conn.commit()
-
+'''	cur.execute('select video_explain from vl_video where video_id = 40')
+	res = cur.fetchone()
+	buf = res['video_explain']
+	print buf
+	if isinstance(buf,unicode):
+		buf = buf.encode('utf8')
+		print buf
+	if isinstance(buf,unicode):
+		print '还是unicode编码'
+'''
 def recvFile(c):
 	fileType,filename = c.recv(24).split(',')
 	#根据文件类型觉定文件的保存路径
